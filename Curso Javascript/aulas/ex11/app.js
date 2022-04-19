477b6a729ce7ce262c95a9e36dce4d254be9e43d
Daniel Dantas <xxxxxdanielzimmxxxx.danieldant@gmail.com>
2022-04-18 23:28:14 -0300

    correções de bugs

--- Curso Javascript/aulas/ex11/app.js
+++ Curso Javascript/aulas/ex11/app.js
@@ -1,16 +1,39 @@
 let numeros = []
+var texto_total = document.getElementById('total_numeros')
 
+function isNumero(n) {
+    if(Number (n) <= 0 || Number (n) > 100) {
+        return false
+    }
+
+    else{
+        return true
+    }
+}
+
+function verifica_inLista(numero_lista, lista = []) {
+    let verifica_numero = lista.indexOf(numero_lista)
+        //Quando o indexOf não encontra o elemento dentro do array ele retorna -1 então se for diferente disso, significa que já foi adicionado
+
+    if(verifica_numero != -1){
+        return true
+    }
+
+    else{
+        return false
+    }
+}
 //Função que é ativa ao apertar no botão adicionar
 function add() {
     var numero = document.getElementById('numero_adiciona')
     
     var texto = document.getElementById('texto')
 
-    let verifica_numero = numeros.indexOf(Number(numero.value)) // verifica se contém um elemento (número) no array
+    texto_total.innerHTML = ''
+     // verifica se contém um elemento (número) no array
 
-    if(verifica_numero != -1){
-        //Quando o indeOf não encontra o elemento dentro do array ele retorna -1 então se for diferente disso, significa que já foi adicionado
-        alert('O número já foi adicionado, tente novamente!')
+    if(isNumero(Number(numero.value)) == false || verifica_inLista(Number(numero.value), numeros) == true){
+        alert('O número já foi adicionado ou inválido, tente novamente!')
     }
     else{
         numeros.push(Number(numero.value))
@@ -21,34 +44,42 @@ function add() {
 
 //Função que é ativa ao apertar no botão finalizar
 
-function verifica() {
+function mostra_texto() {
+    
     var conteudo = document.querySelector('p')
     conteudo.style.padding = '10px'
 
-    var texto_total = document.getElementById('total_numeros')
-    var texto_maior = document.getElementById('maior_numero')
-    var texto_menor = document.getElementById('menor_numero')
-    var texto_soma = document.getElementById('soma_numeros')
-    var texto_media = document.getElementById('media_numeros')
-
-    texto_total.innerText = `Ao todo, temos ${numeros.length} números`
-
-    numeros.sort() // coloca os números em ordem crescente
-    let maior = numeros[0]
-    let menor =  numeros[0]
     let soma = 0
 
+    // let maior = numeros[0]
+    // let menor =  numeros[0]
+    
+    //numeros.sort() // coloca os números em ordem crescente. Sort está dando bug qnd coloca o 100 pois ele deixa na primeira posição
+
+    // for(let i = 0; i < numeros.length; i++){
+    //     if(i + 1 == numeros.length){
+    //         maior = numeros[i]
+    //     }
+
+    //     soma += numeros[i]
+    // }
+
+
+    let maior = numeros[0]
+    let menor =  numeros[0]
+
+
     for(let i = 0; i < numeros.length; i++){
-        if(i + 1 == numeros.length){
+        if(maior < numeros[i]){
             maior = numeros[i]
         }
 
+        if(numeros[i] < menor){
+            menor = numeros[i]
+        }
         soma += numeros[i]
     }
 
-    texto_maior.innerText = `O maior número informado é o ${maior}`
-    texto_menor.innerText = `O menor número informado é o ${menor}`
-    texto_soma.innerText = `Somando todos os valores, temos ${soma}`
-    texto_media.innerText = `A média dos valores digitados é ${soma/numeros.length}`
 
+    texto_total.innerText = `Ao todo, temos ${numeros.length} números. \nO maior número informado é o ${maior} \nO menor número informado é o ${menor} \nSomando todos os valores, temos ${soma} \nA média dos valores digitados é ${soma/numeros.length}`
 }
